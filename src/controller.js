84d78696454b163c79ea476542166bc1fc2e96d8
Emmanuel Mota <emmanuelmota@gmail.com>
2022-05-03 10:56:35 -0700

    Restructures Controller.js. Removes Document ready CONTAINER animation

--- src/controller.js
+++ src/controller.js
@@ -1,43 +1,46 @@
 // Angular.JS
-angular.module("classyPrep", []).controller("classyPrepController", function () {
-  var todoList = this;
-  todoList.todos = [
-    {
-      text: "Demonstrate proficiency with Angular.js and Jquery 👨🏽‍💻",
-      done: false,
-    },
-    { text: "Impress the team 👨🏽‍💻", done: false },
-    { text: "Join the team and hit the ground running 🙌🏽", done: false },
-  ];
-  //Add a task to list
-  todoList.addTodo = function () {
-    if (!todoList.todoText) {
-      alertAnimation("taskTitle");
-    } else {
-      console.log(todoList.todoText);
-      todoList.todos.push({ text: todoList.todoText, done: false });
-      todoList.todoText = "";
-    }
-  };
-  // Return count of remaining tasks
-  todoList.remaining = function () {
-    var count = 0;
-    angular.forEach(todoList.todos, function (todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
-  // Archive tasks that have been checked off
-  todoList.archive = function () {
-    alertAnimation("list");
-    var oldTodos = todoList.todos;
-    todoList.todos = [];
-    angular.forEach(oldTodos, function (todo) {
-      if (!todo.done) todoList.todos.push(todo);
-    });
-  };
-});
-//
+angular
+  .module("classyPrep", [])
+  .controller("classyPrepController", function () {
+    var todoList = this;
+    todoList.todos = [
+      {
+        text: "Demonstrate proficiency with Angular.js and Jquery 👨🏽‍💻",
+        done: false,
+      },
+      { text: "Impress the team 👨🏽‍💻", done: false },
+      { text: "Join the team and hit the ground running 🙌🏽", done: false },
+    ];
+    //Add a task to list
+    todoList.addTodo = function () {
+      if (!todoList.todoText) {
+        alertAnimation("taskTitle");
+      } else {
+        console.log(todoList.todoText);
+        todoList.todos.push({ text: todoList.todoText, done: false });
+        todoList.todoText = "";
+      }
+    };
+    // Return count of remaining tasks
+    todoList.remaining = function () {
+      var count = 0;
+      angular.forEach(todoList.todos, function (todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+    // Archive tasks that have been checked off
+    todoList.archive = function () {
+      alertAnimation("list");
+      var oldTodos = todoList.todos;
+      todoList.todos = [];
+      angular.forEach(oldTodos, function (todo) {
+        if (!todo.done) todoList.todos.push(todo);
+      });
+    };
+  });
+// JQUERY
+//  Alert Animation
 let alertAnimation = (className) => {
   let i = 0;
   while (i < 3) {
@@ -46,13 +49,10 @@ let alertAnimation = (className) => {
     i++;
   }
 };
-// Jquery Animation
+// ID animations
 $(document).ready(function () {
-  console.log("ready!");
-  $("#container").fadeIn("slow");
   $("#logo").click(function () {
-    console.log("Clicked");
-    $("#logo").fadeOut("slow");
-    $("#logo").fadeIn("slow");
+    $("#container").fadeOut("slow");
+    $("#container").fadeIn("slow");
   });
 });
